31abcf4e96a1836acabb361e4ec0055dea86d4a9
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2025-01-01 01:05:48 -0600

    initialized project

---
+++ Algorithms/palindromeNumber.js
@@ -0,0 +1,18 @@
+/**
+ * Given an integer x
+ * return true if x is a 
+ * palindrome, and false otherwise.
+ * 
+ * @param {number} x
+ * @return {boolean}
+ */
+
+var isPalindrome = function(x) {
+    const str = x.toString();
+    const reverseString = str.split('').reverse().join('');
+    return str === reverseString;
+};
+//test case
+console.log(isPalindrome(121)); //true
+console.log(isPalindrome(-121)); //false
+console.log(isPalindrome(10)); //false
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Retos de programación
+---
+ En este repositorio tengo los retos de programación que he hecho de la plataforma de LeetCode para mejorar mi lógica de programación, además de practicar para futuras pruebas técnicas, los cuales se dividen en diferentes dificultades ("Easy, Medium &  Hard").
+
+ ## Estructura de los retos
+
+- Algoritmos
+- Bases de datos
+- Shell
+- Concurrencia 
