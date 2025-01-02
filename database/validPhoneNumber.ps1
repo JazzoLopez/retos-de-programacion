91bc1a28471d21bf7f00c9b39a399bd9b17bebd7
Jazziel Rodríguez López <jazzielrodriguezlopez@gmail.com>
2025-01-02 17:11:40 -0600

    Add: Valid phone number

---
+++ database/validPhoneNumber.ps1
@@ -0,0 +1,16 @@
+# * -> Easy <- *
+# Given a text file file.txt that contains a list of phone numbers (one per line)
+#  write a one-liner bash script to print all valid phone numbers.
+
+# You may assume that a valid phone number must appear in one of the following two 
+# formats: (xxx) xxx-xxxx or xxx-xxx-xxxx. (x means a digit)
+# You may also assume each line in the text file must not contain leading or trailing white spaces.
+
+@"
+987-123-4567
+123 456 7890
+(123) 456-7890
+"@ | Out-File -FilePath file.txt
+
+# Filtrar números válidos
+Select-String -Pattern '^(\([0-9]{3}\) [0-9]{3}-[0-9]{4}|[0-9]{3}-[0-9]{3}-[0-9]{4})$' -Path .\file.txt
